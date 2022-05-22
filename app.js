757e29155620daef27b5d3bb961f4a14fcfd9fbb
jishnupunathil <jishnupunathil000@gmail.com>
2022-05-22 23:28:27 +0530

    lib

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));//part 1#2
 app.use(express.json());
 app.use(express.static(path.join(__dirname , '/public'))); 
+app.use(cors()) //
 
 app.use('/login',loginRouter); 
 app.use('/signup',signupRouter); 
